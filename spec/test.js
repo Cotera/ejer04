3a9944f254f096c96a9f42552d815a3674be2fdd
cursoCIC00 <cursoCIC00@outlook.com>
2017-07-05 18:00:46 +0200

    Primer Commit

---
+++ spec/test.js
@@ -0,0 +1,11 @@
+var miModulo = require ('../index.js');
+
+describe ("Asuite is just a function", function () {
+	var = resultado;
+
+	it ("and so is a espec", function () {
+		resultado = miModulo.sumar(4,5);
+
+		expect(resultado).toBe(9);
+	});
+});
